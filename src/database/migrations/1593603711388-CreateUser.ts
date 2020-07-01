2dfd4b60d51fe7e3a9cc2862fbf35f23782dd524
vhnakai <hugo.nakai@gmail.com>
2020-07-01 10:26:35 -0300

    User Table & Alter Provider to Provider_id on Appointment

--- src/database/migrations/1593603711388-CreateUser.ts
+++ src/database/migrations/1593603711388-CreateUser.ts
@@ -2,11 +2,11 @@
 /* eslint-disable import/prefer-default-export */
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateAppointments1593553607381 implements MigrationInterface {
+export class CreateUser1593603711388 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -16,14 +16,28 @@ export class CreateAppointments1593553607381 implements MigrationInterface {
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'create_at',
             type: 'timestamp',
-            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'update_at',
+            type: 'timestamp',
+            default: 'now()',
           },
         ],
       }),
@@ -31,6 +45,6 @@ export class CreateAppointments1593553607381 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
---
+++ src/database/migrations/1593607295721-alterproviderFieldToProviderId.ts
@@ -0,0 +1,50 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable import/prefer-default-export */
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class alterproviderFieldToProviderId1593607295721
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('appointments', 'provider');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'AppointmentProvider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
+
+    await queryRunner.dropColumn('appointments', 'provider_id');
+
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider',
+        type: 'varchar',
+        isNullable: false,
+      }),
+    );
+  }
+}
