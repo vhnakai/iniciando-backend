4b938f3870773f584ee4a38bbc6d046b8309403f
vhnakai <hugo.nakai@gmail.com>
2020-07-03 11:10:53 -0300

    Global error tratament

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -5,21 +5,17 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(user);
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
