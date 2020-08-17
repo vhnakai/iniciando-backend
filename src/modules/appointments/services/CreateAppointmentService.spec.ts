9f19a4cfb9f1e40e8125f5e9a63ea53e782ca324
vhnakai <36956647+vhnakai@users.noreply.github.com>
2020-08-17 20:49:41 -0300

    fakeRepository

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,16 @@
+import FakeAppointmentRepository from '../repositories/Fakes/FakeAppointmentRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {});
+  it('should be able to create a new appointment', () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    createAppointment.execute({
+      date: new Date(),
+      provider_id: '12311313123',
+    });
+  });
 });
