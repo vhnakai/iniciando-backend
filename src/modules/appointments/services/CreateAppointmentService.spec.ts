a2a82779fc66a95f7b203ec0e55b98da15966d14
vhnakai <36956647+vhnakai@users.noreply.github.com>
2020-08-17 21:51:35 -0300

    coverage&unit-test

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,15 +2,18 @@ import FakeAppointmentRepository from '../repositories/Fakes/FakeAppointmentRepo
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
+  it('should be able to create a new appointment', async () => {
     const fakeAppointmentRepository = new FakeAppointmentRepository();
     const createAppointment = new CreateAppointmentService(
       fakeAppointmentRepository,
     );
 
-    createAppointment.execute({
+    const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: '12311313123',
     });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12311313123');
   });
 });
