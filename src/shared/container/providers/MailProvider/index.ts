0977a41a19966fe7ea1689798ee03a9b5e6e3bc5
Vitor Hugo Nakai <36956647+vhnakai@users.noreply.github.com>
2020-09-01 11:31:04 -0300

    Arrumando os Containers

---
+++ src/shared/container/providers/MailProvider/index.ts
@@ -0,0 +1,16 @@
+import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
+import IMailProvider from './models/IMailProvider';
+
+import EtherealMailProvider from './implementations/EtherealMailProvider';
+import SESMailProvider from './implementations/SESMailProvider';
+
+const provider = {
+  ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider),
+};
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  provider[mailConfig.driver],
+);
