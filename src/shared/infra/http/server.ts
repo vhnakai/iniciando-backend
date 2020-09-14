b492bd91a887335ec9628f2a2eb015411c81a76d
vitor nakai <hugo.nakai@gmail.com>
2020-09-14 16:54:19 -0300

    Update server.ts

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -15,10 +15,10 @@ import ratelimiter from './middlewares/rateLimiter';
 
 const app = express();
 
-app.use(ratelimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(ratelimiter);
 app.use(routes);
 app.use(errors);
 
